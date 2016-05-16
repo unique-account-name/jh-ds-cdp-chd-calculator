a0efdfa70c5243fd094afef888d19129f181e4c4
rruiz <ricey@valeso.com>
2016-05-15 23:17:21 -0400

    Updated layout and instructions

--- server.R
+++ server.R
@@ -1,44 +1,10 @@
 library( shiny )
 library( ggplot2 )
 
-# Define server logic required to draw a histogram
 shinyServer( function( input, output, session ) {
-   
-    #output$foo <- renderPrint( "9135923459" )
-    #output$value <- renderPrint({ input$action })
-    # You can access the value of the widget with input$text, e.g.
-    #output$text1 <- renderPrint({ input$risk_yours * 2 })
-    #print( "asdfasdf" )
-    
-    #updateSliderInput( session, "age", value = 23 )#, min = floor(val/2), max = val+4, step = (val+1)%%2 + 1)
-    
-    #if ( input$health_choices ) {
-        
-        #updateSelectInput( session, "smoker", label = "Current smoker", choices = c( "No", "Yes" ), selected = "Yex" )
-    #}
-    
-    # output$text1 <- renderText({ 
-    #     paste("You have selected mitigating factor ", input$health_choices )
-    # 
-    # })
-    # 
-    # output$text2 <- renderText({ 
-    #     paste( "This your updated risk: ", input$risk_yours )
-    # })
-    # output$text3 <- renderText({ 
-    #     if ( input$smoker == "Yes" ) {
-    #         paste( "You smoke.  Ick!" )
-    #     } else {
-    #         paste( "You don't smoke. Yay!" )
-    #     }
-    # })
-    # output$text4 <- renderText({ 
-    #     #input$action[ 1 ]
-    #     paste( "last widget used: ", input$last_widget_used )
-    # })
     
     output$risk_plot <- renderPlot({
-      
+        
         risk_type <- c( "Yours", "Optimal", "Average" )
         print( input$risk_yours )
         risk <- c( input$risk_yours, input$risk_optimal, input$risk_average )
